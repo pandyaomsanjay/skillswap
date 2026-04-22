5c1c71c50df8de15c0a303bc56b10198a6faff48
Riya Patel <patelriyah837@gmail.com>
2026-04-22 13:47:30 +0530

    Fix formatting in SupabaseClient.kt

--- app/src/main/java/com/example/sgp/SupabaseClient.kt
+++ app/src/main/java/com/example/sgp/SupabaseClient.kt
@@ -9,7 +9,8 @@ object SupabaseClient {
     val client = createSupabaseClient(
         supabaseUrl = "https://ghrxltlstncjcizyyqfo.supabase.co",
         supabaseKey = "sb_publishable_gqJk7xQe8lHqHKKtsCxb7g_txjduIKv"
-    ) {
+    ) 
+    {
         install(Storage)
         install(Postgrest)
         install(Auth)
